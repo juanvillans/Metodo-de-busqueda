c76687df7354488833cae55093e7422e62b08dd7
Juan Francisco Villasmil Tovar <juanvillans16@gmail.com>
2023-06-09 21:12:39 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,66 @@
+const inp_function = document.querySelector('#inp_funcion');
+const inp_xl = document.querySelector('#inp_xl');
+const inp_xu = document.querySelector('#inp_xu');
+const inp_e = document.querySelector('#inp_e');
+const list_result = document.querySelector("#listResult")
+const btn_resolver = document.querySelector('#btn_resolver');
+
+btn_resolver.onclick = (e) => {
+    const transform = inp_function.value.replaceAll('x', 3)
+    list_result.innerHTML = ''
+    metodoDeBusqueda(inp_function.value, +inp_xl.value, +inp_xu.value, +inp_e.value);
+}
+
+// const e = 0.1;
+function metodoDeBusqueda(mathfunction, xl, xu, e) {
+    let i = 0;
+    let a = 0;
+    let b = 0;
+    let x1 = 0;
+    let x2 = 0;
+   
+    while (true) {
+        i++;
+        a = +(0.618 * (xu - xl)).toFixed(4);
+        b = +(0.618 * a).toFixed(4);
+
+        x1 = +(xl + a).toFixed(4);
+        x2 = +(xl + b).toFixed(4);
+
+        
+        let f1 = math.evaluate(mathfunction.replaceAll('x', x1)).toFixed(4)
+        let f2 = math.evaluate(mathfunction.replaceAll('x', x2)).toFixed(4)
+        let caso = f2 > f1 ? 1 : 2;
+
+
+        const li = document.createElement('li')
+        li.classList.add('pb-5')
+        li.innerHTML = `
+            <h2 class="text-xl">Iteración ${i} </h2>
+            <p>a = 0.618 * (${xu} - ${xl}) => <b>${a} </b> </p>
+            <p>b = 0.618 * (${a}) => <b>${b} </b> </p>
+            <p>x1 = ${xl} + ${a} => <b>${x1} </b> </p>
+            <p>x2 = ${xl} + ${b} => <b>${x2} </b> </p>
+            <p>f(x1) = ${mathfunction.replaceAll('x', x1)} => <b>${f1} </b> </p>
+            <p>f(x2) = ${mathfunction.replaceAll('x', x2)} => <b>${f2} </b> </p>
+            <p>caso: <b>#${caso === 1 ? 'caso 1, f2 > 1' : 'caso 2, f2 < f1'}</b> </p>
+
+            <p>Restar x2 - x1: ${x2} - ${x1} = ${(x2-x1).toFixed(4)} ${Math.abs(x2 - x1) < e ? ', ¡El búcle se detiene!' : ', es mayor que e. El bucle continua  '}</p>
+        `
+        list_result.append(li)
+    
+        if (caso == 1) {
+            xu = +x1.toFixed(4);
+        } else {
+            xl = +x2.toFixed(4);
+        }
+        // console.log({x1, x2, f1, f2, caso});
+        // if (i == 100) break;
+        if (Math.abs(x2 - x1) < e) {
+            break;
+        }
+       
+    }
+
+    
+}
